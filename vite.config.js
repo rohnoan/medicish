9ccf1951b0d49da6b8af4438fd0bda73673353ba
rohnoan <sharmarohan2507@gmail.com>
2024-09-08 15:26:38 +0530

    changed vite.congif

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/medicish/'  // This should match your GitHub Pages URL path
 })
