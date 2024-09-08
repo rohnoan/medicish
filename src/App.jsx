58941ca4f05903110ebcd8af21c7186e5f5079fa
rohnoan <sharmarohan2507@gmail.com>
2024-09-08 15:49:50 +0530

    changed routed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-import Logo from './components/Logo'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import MyMeds from './components/MyMeds'
-import Reminder from './components/Reminder'
-import Pharmacies from './components/Pharmacies'
-import Profile from './components/Profile'
-import Logout from './components/Logout'
+// src/App.jsx
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Logo from './components/Logo';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import MyMeds from './components/MyMeds';
+import Reminder from './components/Reminder';
+import Pharmacies from './components/Pharmacies';
+import Profile from './components/Profile';
+import Logout from './components/Logout';
 
-function App(){
+function App() {
   return (
     <div>
-      <Logo/>
-      <Navbar/>
-
+      <Logo />
+      <Navbar />
       <Routes>
-      <Route path='/' element={<Navigate to="/home" />} />
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/my-meds' element={<MyMeds/>}/>
-        <Route path='/reminders' element={<Reminder/>}/>
-        <Route path='/pharmacies' element={<Pharmacies/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/logout' element={<Logout/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/my-meds" element={<MyMeds />} />
+        <Route path="/reminders" element={<Reminder />} />
+        <Route path="/pharmacies" element={<Pharmacies />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
